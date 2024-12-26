e35cf62805d563d355ce31d2716dc68f7f0b85f9
company <jms010802@naver.com>
2024-12-26 16:06:39 +0900

    #6 useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import Button from "./Button"
+import {useState, useEffect} from "react"
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev)=>prev+1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("all Run");
+  useEffect(()=>{
+    console.log("CAll API...");
+  }, []);
+  useEffect(()=>{
+    if(keyword !== "" && keyword.length >5)
+      console.log("SEARCH FOR ", keyword);
+  }, [keyword]);
   return (
     <div>
-      <h1>Welcome back!</h1>
-      <Button text={"Button"}/>
+      <input value={keyword} type="text" placeholder="Search here..." onChange={onChange}/>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }

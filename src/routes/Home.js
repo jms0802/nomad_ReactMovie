6384a4bd602e07226a72f8052b7a0c306b8856c3
company <jms010802@naver.com>
2025-01-06 11:51:39 +0900

    Use router

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import Movie from "./components/Movie";
+import Movie from "../components/Movie";
 
 function Home() {
     const [loading, setLoading] = useState(true);
@@ -33,3 +33,4 @@ function Home() {
         </div>
     );
 }
+export default Home;

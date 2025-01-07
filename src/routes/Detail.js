ebf8be02244daa94e7e12fcae962b8274839e243
PKdorm <jms010802@naver.com>
2025-01-07 23:57:50 +0900

    Detail page

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,39 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 function Detail() {
-    return <h1>Detail</h1>;
+    const [loading, setLoading] = useState(true);
+    const [detail, setDetail] = useState([]);
+    const {id} = useParams();
+    const getMovie = async () => {
+        const json = await (
+            await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+        ).json();
+        setLoading(false);
+        setDetail(json.data.movie); 
+    }
+    useEffect(()=>{
+        getMovie();
+    }, [])
+    return (
+    <div>
+        {loading ? <h1>Detail Loading...</h1> : 
+        <div>
+            <img src={detail.medium_cover_image}/>
+            <h1>{detail.title}</h1>
+            <div>
+                Rating {detail.rating} | 👍{detail.like_count}
+            </div>
+            <hr/>
+            <div>
+                <span>Genres : {detail.genres} | {detail.runtime}min , {detail.language}</span><br/>
+                <span>{detail.year}</span>
+            </div>
+            <div>
+
+            </div>
+        </div>
+        }
+    </div>
+);
 }
 export default Detail;
